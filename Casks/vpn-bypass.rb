6db2b87e68ba19f1e9555f694a4863f8100f3912
Sergio Fernández <9169332+GeiserX@users.noreply.github.com>
2026-04-06 14:31:35 +0200

    bump to v2.2.0

--- Casks/vpn-bypass.rb
+++ Casks/vpn-bypass.rb
@@ -1,6 +1,6 @@
 cask "vpn-bypass" do
-  version "2.1.3"
-  sha256 "7773e6d2256f5669ef35abbcd5998d24669a4f7dd490eb6ed16f3ebf979466a3"
+  version "2.2.0"
+  sha256 "95ed1f0c7925b8a0e4bc9cc3867f08abdcefdb286782301cd4e287e78ddb8e58"
 
   url "https://github.com/GeiserX/VPN-Bypass/releases/download/v#{version}/VPN-Bypass-#{version}.dmg"
   name "VPN Bypass"
